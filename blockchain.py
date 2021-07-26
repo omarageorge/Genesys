52d2f9348bfdd98a39a0ff2be85e660098b9b05e
georgie105 <georgeomara8@gmail.com>
2021-07-26 15:13:46 +0300

    Formatted the blockchain file

--- blockchain.py
+++ blockchain.py
@@ -16,7 +16,7 @@ def update_hash(*args):
 
 
 
-
+# The Block class
 class Block():
     data = None
     hash = None
@@ -30,7 +30,7 @@ class Block():
         self.number = number
         
         
-        
+    # Block Hahsing method   
     def hash(self):
         return update_hash(
             self.previous_hash, 
@@ -40,7 +40,7 @@ class Block():
             )
     
     
-    
+    # Formatted output method
     def __str__(self):
         return str('Block: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s' %(
             self.number, 
@@ -51,7 +51,7 @@ class Block():
             ))
 
 
-
+# Blockchain class
 class Blockchain():
     difficulty = 4
     
@@ -61,32 +61,35 @@ class Blockchain():
         self.chain = chain
 
 
-
+    # Inserts a single block to the blockchain
     def add(self, block):
         self.chain.append(block)
         
         
-        
+    # Deletes a block in the blockchain   
     def remove(self, block):
         self.chain.remove(block)
         
         
-        
+    # Mines a single block    
     def mine(self, block):
         try:
+            # Assign previous Hash value to new block
             block.previous_hash = self.chain[-1].hash()
         except IndexError:
             pass
         
+        # Loops until the 0s at the begining of the block hash equals the difficulty
         while True:
             if block.hash()[:self.difficulty] == '0' * self.difficulty:
                 self.add(block)
                 break
             else:
+                # Keeps tracks of how many times it the sha256 algorithm ran
                 block.nonce += 1
                 
                 
-                
+    # Checks if a block is valid            
     def isValid(self):
         for i in range(1, len(self.chain)):
             _previous = self.chain[i].previous_hash
