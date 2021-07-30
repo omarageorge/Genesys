a5d53e6543c5091d9255c427933515905d4d165d
georgie105 <georgeomara8@gmail.com>
2021-07-30 13:30:42 +0300

    Modified validation code

--- forms.py
+++ forms.py
@@ -6,5 +6,5 @@ class RegisterForm(Form):
     name = StringField('Full Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [validators.Length(min=8), validators.DataRequired(), validators.EqualTo('confirm', message='Passwords do not match')])
+    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords do not match')])
     confirm = PasswordField('Confirm Password')
