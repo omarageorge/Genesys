4d813b13de5a3fc0d14fbad0d4ba7d3b8b1fb626
georgie105 <georgeomara8@gmail.com>
2021-08-05 15:30:49 +0300

    New changes

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template, redirect, url_for, session, request, logging
-from flask.helpers import flash
-from passlib.hash import pbkdf2_sha256
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
 from functools import wraps
 
 from sqlhelpers import *
 from forms import *
+import time
 
 
 app = Flask(__name__)
+
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'omara'
+app.config['MYSQL_PASSWORD'] = 'quantum1'
+app.config['MYSQL_DB'] = 'genesys'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+mysql = MySQL(app)
 
 
 
@@ -18,10 +29,11 @@ def is_logged_in(f):
         if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            flash('Unauthorized, please login.', 'danger')
-            return  redirect(url_for('login'))
-        
+            flash("Unauthorized, please login.", "danger")
+            return redirect(url_for('login'))
     return wrap
+
+
 
 
 def logged_in_redirect(f):
@@ -35,97 +47,160 @@ def logged_in_redirect(f):
     return wrap
 
 
-# MySQL database connection config
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'omara'
-app.config['MYSQL_PASSWORD'] = 'quantum1'
-app.config['MYSQL_DB'] = 'genesys'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = MySQL(app)
-
-
+# Session manager
 def log_in_user(username):
-    users = Table('users', 'name', 'email', 'username', 'password')
-    user = users.getone('username', username)
-    
+    users = Table("users", "name", "email", "username", "password")
+    user = users.getone("username", username)
+
     session['logged_in'] = True
     session['username'] = username
     session['name'] = user.get('name')
     session['email'] = user.get('email')
 
 
-@app.route('/register', methods = ['GET', 'POST'])
+
+
+#Registration page
+@app.route("/register", methods = ['GET', 'POST'])
 @logged_in_redirect
 def register():
-  
     form = RegisterForm(request.form)
-    users = Table('users', 'name', 'email', 'username', 'password')
-    
-    if request.method ==  'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
+    users = Table("users", "name", "email", "username", "password")
+
+    #if form is submitted
+    if request.method == 'POST' and form.validate():
+        #collect form data
         username = form.username.data
-        password = form.password.data
-    
+        email = form.email.data
+        name = form.name.data
+
+        #make sure user does not already exist
         if isnewuser(username):
-            password = pbkdf2_sha256.hash(form.password.data)
-            users.insert(name, email, username, password)
+            #add the user to mysql and log them in
+            password = sha256_crypt.encrypt(form.password.data)
+            users.insert(name,email,username,password)
             log_in_user(username)
             return redirect(url_for('dashboard'))
         else:
-            flash('User already exists', 'danger') 
-            return redirect(url_for('register'))          
-    
+            flash('User already exists', 'danger')
+            return redirect(url_for('register'))
+
     return render_template('register.html', form=form)
 
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+#Login page
+@app.route("/login", methods = ['GET', 'POST'])
 @logged_in_redirect
-def login():   
+def login():
+    #if form is submitted
     if request.method == 'POST':
+        #collect form data
         username = request.form['username']
         candidate = request.form['password']
-        
-        users = Table('users', 'name', 'email', 'username', 'password')
-        user = users.getone('username', username)
-        accpass = user.get('password')
-        
-        if accpass is None:
-            flash('Username is not found', 'danger')
+
+        #access users table to get the user's actual password
+        users = Table("users", "name", "email", "username", "password")
+        user = users.getone("username", username)
+        accPass = user.get('password')
+
+        #if the password cannot be found, the user does not exist
+        if accPass is None:
+            flash("Username is not found", 'danger')
             return redirect(url_for('login'))
         else:
-            if pbkdf2_sha256.verify(candidate, accpass):
+            #verify that the password entered matches the actual password
+            if sha256_crypt.verify(candidate, accPass):
+                #log in the user and redirect to Dashboard page
                 log_in_user(username)
                 flash('You are now logged in.', 'success')
                 return redirect(url_for('dashboard'))
             else:
-                flash('Invalid password', 'danger')
+                #if the passwords do not match
+                flash("Invalid password", 'danger')
                 return redirect(url_for('login'))
-        
+
     return render_template('login.html')
 
 
 
-@app.route('/logout')
+
+#Transaction page
+@app.route("/send", methods = ['GET', 'POST'])
+@is_logged_in
+def transaction():
+    form = SendMoneyForm(request.form)
+    balance = get_balance(session.get('username'))
+
+    #if form is submitted
+    if request.method == 'POST':
+        try:
+            #attempt to execute the transaction
+            send_money(session.get('username'), form.username.data, form.amount.data)
+            flash("Money Sent!", "success")
+        except Exception as e:
+            flash(str(e), 'danger')
+
+        return redirect(url_for('transaction'))
+
+    return render_template('send.html', balance=balance, form=form)
+
+
+
+
+#Buy page
+@app.route("/buy", methods = ['GET', 'POST'])
+@is_logged_in
+def buy():
+    form = BuyForm(request.form)
+    balance = get_balance(session.get('username'))
+
+    if request.method == 'POST':
+        #attempt to buy amount
+        try:
+            send_money("BANK", session.get('username'), form.amount.data)
+            flash("Purchase Successful!", "success")
+        except Exception as e:
+            flash(str(e), 'danger')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('buy.html', balance=balance, form=form)
+
+
+
+
+#logout the user. Ends current session
+@app.route("/logout")
+@is_logged_in
 def logout():
     session.clear()
-    flash('Logout Success', 'success')
+    flash("Logout success", "success")
     return redirect(url_for('login'))
 
 
-@app.route('/dashboard')
+
+
+#Dashboard page
+@app.route("/dashboard")
 @is_logged_in
 def dashboard():
-    return render_template('dashboard.html', session=session)
+    balance = get_balance(session.get('username'))
+    blockchain = get_blockchain().chain
+    ct = time.strftime("%I:%M %p")
+    return render_template('dashboard.html', balance=balance, session=session, ct=ct, blockchain=blockchain)
 
 
-@app.route('/')
+
+
+#Index page
+@app.route("/")
+@logged_in_redirect
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
-
+#Run app
 if __name__ == '__main__':
     app.secret_key = 'secret123'
-    app.run(debug=True, port=3000)
+    app.run(debug = True, port=3000)
